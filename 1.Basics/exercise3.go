33da6800fdd44960339afe790437b41daa0edbb1
09099sunflower <nozimaabduvohidova@gmail.com>
2023-05-01 16:40:29 +0500

    my example

---
+++ 1.Basics/exercise3.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+	var _v int = 123 
+	println(_v)
+
+	var _d int = 234
+	println(_d)
+
+	var _f int = 345
+    println(_f)
+
+	var myname string = "golang"
+	println(myname) 
+
+	var myName string = "qwerty"
+	println(myName)
+
+	var terminal string = "bill gates"
+	println(terminal)
+
+
+}
